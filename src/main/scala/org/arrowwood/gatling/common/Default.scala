51bd9489ceeb28fd10fc3002d765db4ac3903b2f
John Arrowwood <jarrowwx@Johns-MacBook-Air.local>
2019-10-01 15:43:48 -0400

    Took French out of the default language list

--- src/main/scala/org/arrowwood/gatling/common/Default.scala
+++ src/main/scala/org/arrowwood/gatling/common/Default.scala
@@ -16,7 +16,7 @@ object Default {
             .acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
             .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .acceptEncodingHeader("gzip, deflate")
-            .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+            .acceptLanguageHeader("en-us;q=0.5,en;q=0.3,*;q=0.1")
             .disableCaching
             .disableFollowRedirect
 
